22b4dadf6727d2955ef5b5ad15beeae8b9dc94b9
wankhede04 <wankhedevijay04@gmail.com>
2023-07-11 21:29:40 +0530

    feat : added natspec

--- contract-core/src/Bet.sol
+++ contract-core/src/Bet.sol
@@ -1,77 +1,122 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
+
 import "chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./Escrow.sol";
 
 /**
-* @title Bet Contract
-* @notice This contract allows users to bet on the price movement of Bitcoin
-*/
+ * @title Bet Contract
+ * @dev This contract allows users to bet on the price movement of Bitcoin.
+ * Each bet consists of two parties: User A and User B.
+ * The bet amount, expiration time, closing time and other bet parameters are captured in a struct.
+ * The contract uses Chainlink Price Feeds for getting the BTC/USD price and ERC20 token (USDC) for betting.
+ */
 contract Bet {
     struct BetStruct {
-        address userA; // 160 bits
-        address userB; // 160 bits
-        uint256 btcOpenPrice;  // 256 bits
-        uint64 betAmount;      // 64 bits
-        uint32 expirationTime; // 32 bits
-        uint32 closingTime;    // 32 bits
-        bool isLong;           // 8 bits
-        bool active;           // 8 bits
+        address userA; // Address of User A - 160 bits
+        address userB; // Address of User B - 160 bits
+        uint256 btcOpenPrice;  // BTC price at the time of opening the bet - 356 bits
+        uint64 betAmount;  // Amount of bet in USDC - 64 bits
+        uint32 expirationTime;  // Expiration time of the bet offer - 32 bits
+        uint32 closingTime;  // Time when the bet is supposed to close - 32 bits
+        bool isLong;  // Direction of the bet. If true, User A thinks price will be higher at closing time - 8 bitd
+        bool active;  // True if the bet is active, false if it is waiting for a second participant or already closed - 8 bits
     }
 
+    // Instance of Chainlink's BTC/USD price feed contract
+    AggregatorV3Interface internal priceFeed;
+
+    // Instance of Escrow contract to handle USDC deposits and withdrawals
     Escrow public immutable escrow;
-    AggregatorV3Interface internal priceFeed; // Chainlink's price feed contract instance
-    BetStruct[] public bets; // Array of all bets
+
+    // Array of all bets
+    BetStruct[] public bets;
 
     event BetOpened(uint256 betIndex, address indexed userA, uint64 betAmount, bool isLong, uint32 expirationTime, uint32 closingTime);
     event BetJoined(uint256 betIndex, address indexed userB, uint256 openPrice);
     event BetClosed(uint256 betIndex, address indexed winner, uint256 closePrice);
 
     constructor(address _priceFeed, address _USDC) {
-        escrow = new Escrow(_USDC);
         priceFeed = AggregatorV3Interface(_priceFeed);
+        escrow = new Escrow(_USDC);
     }
 
+    /**
+     * @dev Opens a new bet
+     * @param _isLong Direction of the bet
+     * @param _betAmount Amount of bet in USDC
+     * @param _expirationTime Expiration time of the bet offer
+     * @param _closingTime Time when the bet is supposed to close
+     */
     function openBet(bool _isLong, uint64 _betAmount, uint32 _expirationTime, uint32 _closingTime) external {
+        require(_expirationTime <= _closingTime, "Expiration time should be less than or equal to closing time");
+        require(_betAmount > 0, "Bet amount should be greater than zero");
+
         escrow.deposit(msg.sender, _betAmount);
-        require(_expirationTime<=_closingTime,"Expiration time should be less than or equal to closing time");
-        require(_betAmount>0, "Bet amount should be greater than zero");
+
         bets.push(BetStruct(msg.sender, address(0), 0, _betAmount, _expirationTime, _closingTime, _isLong, false));
+
         emit BetOpened(bets.length - 1, msg.sender, _betAmount, _isLong, _expirationTime, _closingTime);
     }
 
+    /**
+     * @dev Joins an existing bet
+     * @param betIndex Index of the bet to join in the `bets` array
+     */
     function joinBet(uint256 betIndex) external {
         BetStruct storage bet = bets[betIndex];
+
         require(!bet.active, "Bet already active");
         require(bet.userA != msg.sender, "Can't join own bet");
         require(block.timestamp <= bet.expirationTime, "Bet expired");
+
         escrow.deposit(msg.sender, bet.betAmount);
+
         (, int256 price,,,) = priceFeed.latestRoundData();
         bet.btcOpenPrice = uint256(price);
         bet.userB = msg.sender;
         bet.active = true;
+
         emit BetJoined(betIndex, msg.sender, bet.btcOpenPrice);
     }
 
+    /**
+     * @dev Closes a bet, sends the funds to the winner
+     * @param betIndex Index of the bet to close in the `bets` array
+     */
     function closeBet(uint256 betIndex) external {
         BetStruct storage bet = bets[betIndex];
+
         require(bet.active, "Bet not active");
         require(block.timestamp >= bet.closingTime, "Bet not yet closed");
+
         (, int256 price,,,) = priceFeed.latestRoundData();
         uint256 closePrice = uint256(price);
+
         address winner = closePrice > bet.btcOpenPrice ? (bet.isLong ? bet.userA : bet.userB) : (bet.isLong ? bet.userB : bet.userA);
-        escrow.release(winner,bet.betAmount*2);
+
+        escrow.release(winner, bet.betAmount * 2);
         bet.active = false;
+
         emit BetClosed(betIndex, winner, closePrice);
     }
 
+    /**
+     * @dev Returns information about a specific bet
+     * @param betIndex Index of the bet in the `bets` array
+     */
     function getBetInfo(uint256 betIndex) external view returns (address userA, address userB, uint256 btcOpenPrice, uint64 betAmount, uint32 expirationTime, uint32 closingTime, bool isLong, bool active) {
         require(betIndex < bets.length, "Bet does not exist");
+
         BetStruct storage bet = bets[betIndex];
+
         return (bet.userA, bet.userB, bet.btcOpenPrice, bet.betAmount, bet.expirationTime, bet.closingTime, bet.isLong, bet.active);
     }
 
-    function totalBets() external view returns(uint256) {
+    /**
+     * @dev Returns total number of bets
+     */
+    function totalBets() external view returns (uint256) {
         return bets.length;
     }
 }
