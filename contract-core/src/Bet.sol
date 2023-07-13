452f76aac3f58d534af7f2200954f6c453d8c418
wankhede04 <wankhedevijay04@gmail.com>
2023-07-13 11:57:25 +0530

    feat : tests accounting for caller reward and draw situation

--- contract-core/src/Bet.sol
+++ contract-core/src/Bet.sol
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
-
 import "chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./Escrow.sol";
 import "openzeppelin-contracts/contracts/access/Ownable.sol";
-
 /**
  * @title Bet Contract
  * @dev This contract allows users to bet on the price movement of Bitcoin.
@@ -23,32 +21,20 @@ contract Bet is Ownable{
         bool isLong;  // Direction of the bet. If true, User A thinks price will be higher at closing time - 8 bitd
         bool active;  // True if the bet is active, false if it is waiting for a second participant or already closed - 8 bits
     }
-
     // Instance of Chainlink's BTC/USD price feed contract
     AggregatorV3Interface internal priceFeed;
-
     // Instance of Escrow contract to handle USDC deposits and withdrawals
     Escrow public immutable escrow;
-
     // Array of all bets
     BetStruct[] public bets;
-
     uint256 public callerRewardPercentage = 1e16;  // 1% initially, represented in a fixed-point format where 1e18 is 100%
-
     event BetOpened(uint256 betIndex, address indexed userA, uint64 betAmount, bool isLong, uint32 expirationTime, uint32 closingTime);
     event BetJoined(uint256 betIndex, address indexed userB, uint256 openPrice);
     event BetClosed(uint256 betIndex, address indexed winner, uint256 closePrice);
-
     constructor(address _priceFeed, address _USDC) {
         priceFeed = AggregatorV3Interface(_priceFeed);
         escrow = new Escrow(_USDC);
     }
-
-    function setCallerRewardPercentage(uint256 newPercentage) external onlyOwner {
-        require(newPercentage <= 1e18, "Reward percentage cannot be more than 100%");
-        callerRewardPercentage = newPercentage;
-    }
-
     /**
      * @dev Opens a new bet
      * @param _isLong Direction of the bet
@@ -59,84 +45,66 @@ contract Bet is Ownable{
     function openBet(bool _isLong, uint64 _betAmount, uint32 _expirationTime, uint32 _closingTime) external {
         require(_expirationTime <= _closingTime, "Expiration time should be less than or equal to closing time");
         require(_betAmount > 0, "Bet amount should be greater than zero");
-
         escrow.deposit(msg.sender, _betAmount);
-
         bets.push(BetStruct(msg.sender, address(0), 0, _betAmount, _expirationTime, _closingTime, _isLong, false));
-
         emit BetOpened(bets.length - 1, msg.sender, _betAmount, _isLong, _expirationTime, _closingTime);
     }
-
     /**
      * @dev Joins an existing bet
      * @param betIndex Index of the bet to join in the `bets` array
      */
     function joinBet(uint256 betIndex) external {
         BetStruct storage bet = bets[betIndex];
-
         require(!bet.active, "Bet already active");
         require(bet.userA != msg.sender, "Can't join own bet");
         require(block.timestamp <= bet.expirationTime, "Bet expired");
-
         escrow.deposit(msg.sender, bet.betAmount);
-
         (, int256 price,,,) = priceFeed.latestRoundData();
         bet.btcOpenPrice = uint256(price);
         bet.userB = msg.sender;
         bet.active = true;
-
         emit BetJoined(betIndex, msg.sender, bet.btcOpenPrice);
     }
-
     /**
      * @dev Closes a bet, sends the funds to the winner
-     * @param betIndex Index of the bet to close in the `bets` array
+     * @param newPercentage The new reward percentage value for the caller of closingBet.
      */
     function setCallerRewardPercentage(uint256 newPercentage) external onlyOwner {
         require(newPercentage <= 1e18, "Reward percentage cannot be more than 100%");
         callerRewardPercentage = newPercentage;
     }
-
     function closeBet(uint256 betIndex) external {
         BetStruct storage bet = bets[betIndex];
-
         require(bet.active, "Bet not active");
         require(block.timestamp >= bet.closingTime, "Bet not yet closed");
-
         (, int256 price,,,) = priceFeed.latestRoundData();
         uint256 closePrice = uint256(price);
         uint256 callerReward = bet.betAmount * 2 * callerRewardPercentage / 1e18;  // Calculate the caller's reward based on the percentage
         uint256 payout = bet.betAmount * 2 - callerReward;  // The remaining after subtracting the caller's reward
-
         if(closePrice == bet.btcOpenPrice) {
             // Draw case, return betAmount to each user
             escrow.release(bet.userA, bet.betAmount - callerReward/2); // deduct half of the caller reward from each user
             escrow.release(bet.userB, bet.betAmount - callerReward/2);
+            emit BetClosed(betIndex, address(0), closePrice);
         } else {
             // Normal case, winner takes all (after subtracting the caller's reward)
             address winner = closePrice > bet.btcOpenPrice ? (bet.isLong ? bet.userA : bet.userB) : (bet.isLong ? bet.userB : bet.userA);
             escrow.release(winner, payout);
             emit BetClosed(betIndex, winner, closePrice);
         }
-
         // Reward the caller
         escrow.release(msg.sender, callerReward);
-
         bet.active = false;
     }
-
     /**
      * @dev Returns information about a specific bet
      * @param betIndex Index of the bet in the `bets` array
      */
     function getBetInfo(uint256 betIndex) external view returns (address userA, address userB, uint256 btcOpenPrice, uint64 betAmount, uint32 expirationTime, uint32 closingTime, bool isLong, bool active) {
         require(betIndex < bets.length, "Bet does not exist");
-
         BetStruct storage bet = bets[betIndex];
-
         return (bet.userA, bet.userB, bet.btcOpenPrice, bet.betAmount, bet.expirationTime, bet.closingTime, bet.isLong, bet.active);
     }
-
     /**
      * @dev Returns total number of bets
      */
--- contract-core/test/Bet.t.sol
+++ contract-core/test/Bet.t.sol
@@ -1,41 +1,36 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.17;
-
 import "forge-std/Test.sol";
-
 import {Bet, Escrow} from "src/Bet.sol";
 import {MockPriceFeed} from "./Mock/MockPriceFeed.sol";
 import {MockUSDC} from "./Mock/MockUSDC.sol";
 import "forge-std/Vm.sol";
-
 contract BetTest is Test {
-
     Bet public bet;
     Escrow public escrow;
     MockPriceFeed public priceFeed;
     MockUSDC public usdc;
     address public  MOCK_ADDR_1 = vm.addr(1);
     address public  MOCK_ADDR_2 = vm.addr(2);
+    uint256 public callerRewardPercentage;
     uint32 constant UNIT_TEST_EXPIRY_TIME = 3600;
     uint32 constant UNIT_TEST_CLOSING_TIME = 7776000;
-
     //--------EVENTS--------//
     event BetOpened(uint256 betIndex, address indexed userA, uint64 betAmount, bool isLong, uint32 expirationTime, uint32 closingTime);
     event BetJoined(uint256 betIndex, address indexed userB, uint256 openPrice);
     event BetClosed(uint256 betIndex, address indexed winner, uint256 closePrice);
-
     //--------SETUP--------//
     function setUp() public {
         priceFeed = new MockPriceFeed();
         usdc = new MockUSDC("Mock USDC", "USDC");
         bet = new Bet(address(priceFeed), address(usdc));
         escrow = bet.escrow();
+        callerRewardPercentage = bet.callerRewardPercentage();
         vm.prank(MOCK_ADDR_1);
         usdc.mint(1e24);
         vm.prank(MOCK_ADDR_2);
         usdc.mint(1e24);
     }
-
     // Test the opening of a bet
     function testOpenBet(bool long, uint64 betAmount, uint16 utExpiry, uint16 utClosing) public {
         vm.assume(betAmount>0);
@@ -45,7 +40,6 @@ contract BetTest is Test {
         // Opening a bet and checking if all parameters are correct
         _openBet(long, betAmount, expiry, closingTime, MOCK_ADDR_1);
     }
-
     // Test the opening of a bet with invalid expiry time
     function testOpenBet_InvalidExpiryTime(bool long, uint64 betAmount, uint16 utExpiry, uint16 utClosing) public {
         vm.assume(betAmount>0);
@@ -55,7 +49,6 @@ contract BetTest is Test {
         // Trying to open a bet with invalid expiry time
         _openBet(long, betAmount, expiry, closingTime, MOCK_ADDR_1);
     }
-
     // Test the joining of a bet
     function testJoinBet(bool long, uint64 betAmount, uint16 utExpiry, uint16 utClosing, uint16 joinTime) public {
         vm.assume(betAmount>0);
@@ -68,7 +61,6 @@ contract BetTest is Test {
         vm.warp(block.timestamp+joinTime);
         _joinBet(betIndex, betAmount, MOCK_ADDR_2);
     }
-
     // Test the joining of a bet after it has expired
     function testJoinBetAfterExpiry(bool long, uint64 betAmount, uint16 utExpiry, uint16 utClosing, uint16 joinTime) public {
         vm.assume(betAmount>0);
@@ -79,14 +71,12 @@ contract BetTest is Test {
         uint256 betIndex = _openBet(long, betAmount, expiry, closingTime, MOCK_ADDR_1);
         // Increasing time to exceed expiry
         vm.warp(block.timestamp + joinTime);
-
         vm.startPrank(MOCK_ADDR_2);
         usdc.approve(address(escrow), betAmount);
         vm.expectRevert(); // bet expired
         bet.joinBet(betIndex);
         vm.stopPrank();
     }
-
     // Test the joining of a bet after it has been closed
     function testJoinBetAfterBetClosed(bool long, uint32 betAmount, uint16 utExpiry, uint16 utClosing, uint joiningTime) public {
         vm.assume(betAmount>0);
@@ -108,7 +98,6 @@ contract BetTest is Test {
         bet.joinBet(betIndex);
         vm.stopPrank();
     }
-
     // Test the closing of a bet
     function testCloseBet(bool long, uint32 betAmount, uint16 utExpiry, uint16 utClosing, uint joiningTime) public {
         vm.assume(betAmount>0);
@@ -124,7 +113,6 @@ contract BetTest is Test {
         vm.warp(block.timestamp + utClosing);
         _closeBet(betIndex, openingPrice, betAmount, MOCK_ADDR_1, MOCK_ADDR_2, long);
     }
-
     // Test the closing of a bet after it has been closed
     function testCLoseBetAfterBetClosed(bool long, uint32 betAmount, uint16 utExpiry, uint16 utClosing, uint joiningTime) public {
         vm.assume(betAmount>0);
@@ -145,7 +133,6 @@ contract BetTest is Test {
         bet.closeBet(betIndex);
         vm.stopPrank();
     }
-
     function _openBet(bool long, uint64 betAmount, uint32 expiry, uint32 closingTime, address maker) internal returns(uint256) {
         if(expiry>closingTime){
             vm.startPrank(maker);
@@ -172,7 +159,6 @@ contract BetTest is Test {
         );
         return length;
     }
-
     function _joinBet(uint256 betIndex, uint64 betAmount, address taker) internal returns(uint256) {
         int256 price = 1e21;
         priceFeed.storePrice(price);
@@ -192,27 +178,51 @@ contract BetTest is Test {
         );
         return uint256(price);
     }
-
     function _closeBet(uint256 betIndex, uint256 openingPrice, uint64 betAmount, address maker, address taker, bool isMakerLong) internal {
         uint256 price = 2e21;
         priceFeed.storePrice(int256(price));
         address winner;
-        if(price > openingPrice) {
-            winner = isMakerLong ? maker : taker;
+        uint256 expectedBalanceIncrease;
+        uint256 reward = 2 * betAmount * callerRewardPercentage / 1e18;
+        if(price == openingPrice) {
+            uint256 makerBalanceBefore = usdc.balanceOf(maker);
+            uint256 takerBalanceBefore = usdc.balanceOf(taker);
+            expectedBalanceIncrease = betAmount - (reward / 2);
+            vm.expectEmit(true, true, true, true);
+            emit BetClosed(betIndex, address(0), price);
+            bet.closeBet(betIndex);
+            uint256 makerBalanceAfter = usdc.balanceOf(maker);
+            uint256 takerBalanceAfter = usdc.balanceOf(taker);
+            assertEq(
+                makerBalanceAfter,
+                makerBalanceBefore + expectedBalanceIncrease,
+                "USDC balance after closing bet is not correct"
+            );
+            assertEq(
+                takerBalanceAfter,
+                takerBalanceBefore + expectedBalanceIncrease,
+                "USDC balance after closing bet is not correct"
+            );
         }
         else {
-            winner = isMakerLong ? taker : maker;
+            if(price > openingPrice) {
+                winner = isMakerLong ? maker : taker;
+            }
+            else {
+                winner = isMakerLong ? taker : maker;
+            }
+
+            expectedBalanceIncrease = betAmount * 2 - reward;
+            uint256 winnerBalanceBefore = usdc.balanceOf(winner);
+            vm.expectEmit(true, true, true, true);
+            emit BetClosed(betIndex, winner, price);
+            bet.closeBet(betIndex);
+            uint256 winnerBalanceAfter = usdc.balanceOf(winner);
+            assertEq(
+                winnerBalanceAfter,
+                winnerBalanceBefore + expectedBalanceIncrease,
+                "USDC balance after closing bet is not correct"
+            );
         }
-        uint256 winnerBalanceBefore = usdc.balanceOf(winner);
-        uint256 expectedBalanceIncrease = betAmount * 2;
-        vm.expectEmit(true, true, true, true);
-        emit BetClosed(betIndex, winner, price);
-        bet.closeBet(betIndex);
-        uint256 winnerBalanceAfter = usdc.balanceOf(winner);
-        assertEq(
-            winnerBalanceAfter,
-            winnerBalanceBefore + expectedBalanceIncrease,
-            "USDC balance after closing bet is not correct"
-        );
     }
 }
